55fda0c8a52a911bc6f01a14679f27ce2e3b7128
StanimalTheMan <sjc591@nyu.edu>
2020-04-04 22:24:57 -0400

    have player data persist after refresh

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,32 +3,32 @@ import { FETCH_BATTER, FETCH_PITCHER } from "./types";
 import axios from "axios";
 
 //use local host in get if you want to run app locally
-export const fetchBatter = formValues => async dispatch => {
+export const fetchBatter = (formValues) => async (dispatch) => {
   axios
-    .get("https://nameless-scrubland-88143.herokuapp.com/batting/player", {
+    .get("http://localhost:3000/batting/player", {
       params: {
         firstname: formValues.firstName,
-        lastname: formValues.lastName
-      }
+        lastname: formValues.lastName,
+      },
     })
     .then(
-      json => dispatch({ type: FETCH_BATTER, payload: json.data }),
+      (json) => dispatch({ type: FETCH_BATTER, payload: json.data }),
       history.push(
         `/batting/player?firstname=${formValues.firstName}&lastname=${formValues.lastName}`
       )
     );
 };
 
-export const fetchPitcher = formValues => async dispatch => {
+export const fetchPitcher = (formValues) => async (dispatch) => {
   axios
-    .get("https://nameless-scrubland-88143.herokuapp.com/pitching/player", {
+    .get("http://localhost:3000/pitching/player", {
       params: {
         firstname: formValues.firstName,
-        lastname: formValues.lastName
-      }
+        lastname: formValues.lastName,
+      },
     })
     .then(
-      json => dispatch({ type: FETCH_PITCHER, payload: json.data }),
+      (json) => dispatch({ type: FETCH_PITCHER, payload: json.data }),
       history.push(
         `/pitching/player?firstname=${formValues.firstName}&lastname=${formValues.lastName}`
       )
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 import reduxThunk from "redux-thunk";
 import "tachyons";
 
@@ -10,14 +13,24 @@ import App from "./components/App";
 import reducers from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 const store = createStore(
-  reducers,
+  persistedReducer,
   composeEnhancers(applyMiddleware(reduxThunk))
 );
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.querySelector("#root")
 );
