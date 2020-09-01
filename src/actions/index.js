60f46c0c6007665f2dbc90b6f2801e6a6f69a08c
StanimalTheMan <sjc591@nyu.edu>
2020-09-01 14:04:03 -0400

    take into account names like CC Sabathia, Hyun-Jin Ryu, other names TBD user burdens possible

--- src/actions/index.js
+++ src/actions/index.js
@@ -97,7 +97,6 @@ export const fetchBatter = (formValues) => async (dispatch) => {
   const requestGeneral = axios.get(
     "https://nameless-scrubland-88143.herokuapp.com/batting/player",
     {
-      // trying to deal with names like CC Sabathia
       params: {
         firstname: formValues.firstName,
         lastname: formValues.lastName,
@@ -230,41 +229,108 @@ export const fetchPitcher = (formValues) => async (dispatch) => {
     }
   );
 
-  //  open to take into account other names TBD
-
-  axios.all([requestOne, requestTwo, requestThree]).then(
-    axios.spread((...responses) => {
-      const responseOne = responses[0];
-      const responseTwo = responses[1];
-      const responseThree = responses[2];
-
-      const toDispatch = responseOne.data.length
-        ? responseOne
-        : responseTwo.data.length
-        ? responseTwo
-        : responseThree;
-
-      firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
-      if (toDispatch === responseOne) {
-        lastName = formValues.lastName.replace(/\s/g, "").toLowerCase();
-        lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
-      } else if (toDispatch === responseTwo) {
-        lastName =
-          lastName.slice(0, 2) +
-          lastName.charAt(2).toUpperCase() +
-          lastName.slice(3);
-      } else {
-        lastName =
-          lastName.charAt(0).toUpperCase() +
-          lastName.charAt(1) +
-          lastName.charAt(2).toUpperCase() +
-          lastName.slice(3);
-      }
-
-      dispatch({ type: FETCH_PITCHER, payload: toDispatch.data });
-      history.push(
-        `/pitching/player?firstname=${firstName}&lastname=${lastName}`
-      );
-    })
+  const requestFour = axios.get(
+    "https://nameless-scrubland-88143.herokuapp.com/pitching/player",
+    {
+      // trying to deal with names like CC Sabathia
+      params: {
+        firstname: firstName.toUpperCase(),
+        lastname: lastName.charAt(0).toUpperCase() + lastName.slice(1),
+      },
+    }
   );
+
+  const requestFive = axios.get(
+    "https://nameless-scrubland-88143.herokuapp.com/pitching/player",
+    {
+      // trying to deal with names like Hyun-Jin Ryu (although it might not be user friendly to mandate the - in Hyun-Jin from users)
+      params: {
+        firstname:
+          firstName.charAt(0).toUpperCase() +
+          firstName.slice(1, 5) +
+          firstName.charAt(5).toUpperCase() +
+          firstName.slice(6),
+        lastname: lastName.charAt(0).toUpperCase() + lastName.slice(1),
+      },
+    }
+  );
+
+  //  open to take into account other names TBD (hard to match all kinds of names such as de La Rosa as opposed to De la Rosa as oppposed to J. D. Davis)
+  //  will at the moment make users accountable for inputtign exact name as it is in database (needs improvement)
+  const requestGeneral = axios.get(
+    "https://nameless-scrubland-88143.herokuapp.com/pitching/player",
+    {
+      params: {
+        firstname: formValues.firstName,
+        lastname: formValues.lastName,
+      },
+    }
+  );
+
+  axios
+    .all([
+      requestOne,
+      requestTwo,
+      requestThree,
+      requestFour,
+      requestFive,
+      requestGeneral,
+    ])
+    .then(
+      axios.spread((...responses) => {
+        const responseOne = responses[0];
+        const responseTwo = responses[1];
+        const responseThree = responses[2];
+        const responseFour = responses[3];
+        const responseFive = responses[4];
+        const responseGeneral = responses[5];
+
+        const toDispatch = responseOne.data.length
+          ? responseOne
+          : responseTwo.data.length
+          ? responseTwo
+          : responseThree.data.length
+          ? responseThree
+          : responseFour.data.length
+          ? responseFour
+          : responseFive.data.length
+          ? responseFive
+          : responseGeneral;
+
+        firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+        if (toDispatch === responseOne) {
+          lastName = formValues.lastName.replace(/\s/g, "").toLowerCase();
+          lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+        } else if (toDispatch === responseTwo) {
+          lastName =
+            lastName.slice(0, 2) +
+            lastName.charAt(2).toUpperCase() +
+            lastName.slice(3);
+        } else if (toDispatch === responseThree) {
+          lastName =
+            lastName.charAt(0).toUpperCase() +
+            lastName.charAt(1) +
+            lastName.charAt(2).toUpperCase() +
+            lastName.slice(3);
+        } else if (toDispatch === responseFour) {
+          firstName = firstName.toUpperCase();
+          lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+        } else if (toDispatch === responseFive) {
+          firstName =
+            firstName.charAt(0).toUpperCase() +
+            firstName.slice(1, 5) +
+            firstName.charAt(5).toUpperCase() +
+            firstName.slice(6);
+          lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+        } else {
+          firstName = formValues.firstName;
+          lastName = formValues.lastName;
+        }
+
+        dispatch({ type: FETCH_PITCHER, payload: toDispatch.data });
+        history.push(
+          `/pitching/player?firstname=${firstName}&lastname=${lastName}`
+        );
+      })
+    );
 };
