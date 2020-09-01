b5e76c1f6d03f948a505e3835ae467884338d904
StanimalTheMan <sjc591@nyu.edu>
2020-09-01 13:42:41 -0400

    take into account names like CC Sabathia, Hyun-JIn Ryu and user dependent batter queries

--- src/actions/index.js
+++ src/actions/index.js
@@ -39,7 +39,7 @@ export const fetchBatter = (formValues) => async (dispatch) => {
   const requestThree = axios.get(
     "https://nameless-scrubland-88143.herokuapp.com/batting/player",
     {
-      // trying to deal with names like Jacob deGrom
+      // trying to deal with names like Jacob deGrom and Travis d'Arnaud
       params: {
         firstname: firstName.charAt(0) + firstName.slice(1).toLowerCase(),
         lastname:
@@ -65,56 +65,125 @@ export const fetchBatter = (formValues) => async (dispatch) => {
     }
   );
 
-  //  open to take into account other names TBD
-
-  axios.all([requestOne, requestTwo, requestThree, requestFour]).then(
-    axios.spread((...responses) => {
-      const responseOne = responses[0];
-      const responseTwo = responses[1];
-      const responseThree = responses[2];
-      const responseFour = responses[3];
-
-      const toDispatch = responseOne.data.length
-        ? responseOne
-        : responseTwo.data.length
-        ? responseTwo
-        : responseThree.data.length
-        ? responseThree
-        : responseFour;
-
-      if (toDispatch === responseOne) {
-        firstName = formValues.firstName.replace(/\s/g, "").toLowerCase();
-        firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
-        lastName = formValues.lastName.replace(/\s/g, "").toLowerCase();
-        lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
-      } else if (toDispatch === responseTwo) {
-        firstName = formValues.firstName.replace(/\s/g, "").toUpperCase();
-        lastName = formValues.lastName.replace(/\s/g, "").toUpperCase();
-        lastName =
-          lastName.charAt(0) +
-          lastName.charAt(1).toLowerCase() +
-          lastName.charAt(2) +
-          lastName.slice(3).toLowerCase();
-      } else if (toDispatch === responseThree) {
-        firstName = firstName.charAt(0) + firstName.slice(1).toLowerCase();
-        lastName =
-          lastName.slice(0, 2).toLowerCase() +
-          lastName.charAt(2) +
-          lastName.slice(3).toLowerCase();
-      } else {
-        firstName = firstName.charAt(0) + firstName.slice(1).toLowerCase();
-        lastName =
-          lastName.charAt(0) +
-          lastName.charAt(1).toLowerCase() +
-          lastName.charAt(2) +
-          lastName.slice(3).toLowerCase();
-      }
-      dispatch({ type: FETCH_BATTER, payload: toDispatch.data });
-      history.push(
-        `/batting/player?firstname=${firstName}&lastname=${lastName}`
-      );
-    })
+  const requestFive = axios.get(
+    "https://nameless-scrubland-88143.herokuapp.com/batting/player",
+    {
+      // trying to deal with names like Hyun-Jin Ryu
+      params: {
+        firstname:
+          firstName.charAt(0) +
+          firstName.slice(1, 4).toLowerCase() +
+          firstName.slice(4, 6) +
+          firstName.slice(6).toLowerCase(),
+        lastname: lastName.charAt(0) + lastName.slice(1).toLowerCase(),
+      },
+    }
   );
+
+  const requestSix = axios.get(
+    "https://nameless-scrubland-88143.herokuapp.com/batting/player",
+    {
+      // trying to deal with names like CC Sabathia
+      params: {
+        firstname: firstName,
+        lastname: lastName.charAt(0) + lastName.slice(1).toLowerCase(),
+      },
+    }
+  );
+
+  //  open to take into account other names TBD (hard to match all kinds of names such as de La Rosa as opposed to De la Rosa as oppposed to J. D. Davis)
+  //  will at the moment make users accountable for inputtign exact name as it is in database (needs improvement)
+
+  const requestGeneral = axios.get(
+    "https://nameless-scrubland-88143.herokuapp.com/batting/player",
+    {
+      // trying to deal with names like CC Sabathia
+      params: {
+        firstname: formValues.firstName,
+        lastname: formValues.lastName,
+      },
+    }
+  );
+
+  axios
+    .all([
+      requestOne,
+      requestTwo,
+      requestThree,
+      requestFour,
+      requestFive,
+      requestSix,
+      requestGeneral,
+    ])
+    .then(
+      axios.spread((...responses) => {
+        const responseOne = responses[0];
+        const responseTwo = responses[1];
+        const responseThree = responses[2];
+        const responseFour = responses[3];
+        const responseFive = responses[4];
+        const responseSix = responses[5];
+        const responseGeneral = responses[6];
+
+        const toDispatch = responseOne.data.length
+          ? responseOne
+          : responseTwo.data.length
+          ? responseTwo
+          : responseThree.data.length
+          ? responseThree
+          : responseFour.data.length
+          ? responseFour
+          : responseFive.data.length
+          ? responseFive
+          : responseSix.data.length
+          ? responseSix
+          : responseGeneral;
+
+        if (toDispatch === responseOne) {
+          firstName = formValues.firstName.replace(/\s/g, "").toLowerCase();
+          firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+          lastName = formValues.lastName.replace(/\s/g, "").toLowerCase();
+          lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+        } else if (toDispatch === responseTwo) {
+          firstName = formValues.firstName.replace(/\s/g, "").toUpperCase();
+          lastName = formValues.lastName.replace(/\s/g, "").toUpperCase();
+          lastName =
+            lastName.charAt(0) +
+            lastName.charAt(1).toLowerCase() +
+            lastName.charAt(2) +
+            lastName.slice(3).toLowerCase();
+        } else if (toDispatch === responseThree) {
+          firstName = firstName.charAt(0) + firstName.slice(1).toLowerCase();
+          lastName =
+            lastName.slice(0, 2).toLowerCase() +
+            lastName.charAt(2) +
+            lastName.slice(3).toLowerCase();
+        } else if (toDispatch === responseFour) {
+          firstName = firstName.charAt(0) + firstName.slice(1).toLowerCase();
+          lastName =
+            lastName.charAt(0) +
+            lastName.charAt(1).toLowerCase() +
+            lastName.charAt(2) +
+            lastName.slice(3).toLowerCase();
+        } else if (toDispatch === responseFive) {
+          firstName =
+            firstName.charAt(0) +
+            firstName.slice(1, 4).toLowerCase() +
+            firstName.slice(4, 6) +
+            firstName.slice(6).toLowerCase();
+          lastName = lastName.charAt(0) + lastName.slice(1).toLowerCase();
+        } else if (toDispatch === responseSix) {
+          lastName = lastName.charAt(0) + lastName.slice(1).toLowerCase();
+        } else {
+          firstName = formValues.firstName;
+          lastName = formValues.lastName;
+        }
+        dispatch({ type: FETCH_BATTER, payload: toDispatch.data });
+        history.push(
+          `/batting/player?firstname=${firstName}&lastname=${lastName}`
+        );
+      })
+    );
 };
 
 export const fetchPitcher = (formValues) => async (dispatch) => {
